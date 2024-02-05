e93417af20f25ee79ce7a8c585b1af9e1cbccdd2
Victor Escandiel <joaoescandielb@gmail.com>
2024-02-05 15:40:51 -0300

    fix: refactoring the logic and applied a mock data with a map method in the li element

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,28 +1,27 @@
 import React from "react";
 
-export default function List () {
-    return (
-        <aside>
-            <h2>Studies of the day</h2>
-            <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>
-                    02:00:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        Javascript
-                    </h3>
-                    <span>
-                    01:00:00
-                    </span>
-                </li>
-            </ul>        
-        </aside>
-    )
+export default function List() {
+  const tasks = [
+    {
+      task: "React",
+      time: "02:00:00",
+    },
+    {
+      task: "Javascript",
+      time: "01:00:00",
+    },
+  ];
+  return (
+    <aside>
+      <h2>Studies of the day</h2>
+      <ul>
+        {tasks.map((item, index) => (
+          <li key={index}>
+            <h3>{item.task}</h3>
+            <span>{item.time}</span>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
-
