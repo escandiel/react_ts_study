b923f991f87cf580fd92dca93f192d70369303ec
Victor Escandiel <joaoescandielb@gmail.com>
2024-02-05 15:29:47 -0300

    feat: aside done

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 
-class List extends React.Component {
-  render() {
-    return <div></div>;
-  }
+export default function List () {
+    return (
+        <aside>
+            <h2>Studies of the day</h2>
+            <ul>
+                <li>
+                    <h3>
+                        React
+                    </h3>
+                    <span>
+                    02:00:00
+                    </span>
+                </li>
+                <li>
+                    <h3>
+                        Javascript
+                    </h3>
+                    <span>
+                    01:00:00
+                    </span>
+                </li>
+            </ul>        
+        </aside>
+    )
 }
 
-export default List;
