a1e9449e5f52ca2bea872f0f89744d50402cc8a8
Victor Escandiel <joaoescandielb@gmail.com>
2024-02-07 19:57:20 -0300

    fix: changing all the scss files to scss module files

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,6 @@
 import Form from "../components/Form";
 import List from "../components/List";
-import style from "./style.module.scss";
+import style from "./App.module.scss";
 
 function App() {
   return (
