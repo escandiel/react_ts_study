606ade0437695f5b1d939cdedfa1a5926e8cf3d6
Victor Escandiel <joaoescandielb@gmail.com>
2024-02-08 17:15:09 -0300

    fix: button props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./style.scss";
 
-class Button extends React.Component {
+class Button extends React.Component<any, any> {
   render() {
-    return <button className="botao">Button</button>;
+    return <button className="botao">{this.props.children}</button>;
   }
 }
 
