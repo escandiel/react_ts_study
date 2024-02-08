43a80359a8e905b3380ec77c3ae0b0d11a9e724d
Victor Escandiel <joaoescandielb@gmail.com>
2024-02-08 19:26:00 -0300

    feat: add styles for Stopwatch and its components

---
+++ src/components/Stopwatch/Watch/index.tsx
@@ -0,0 +1,11 @@
+export default function Watch() {
+  return (
+    <>
+      <span>0</span>
+      <span>0</span>
+      <span>:</span>
+      <span>0</span>
+      <span>0</span>
+    </>
+  );
+}
--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -1,14 +1,18 @@
 import Button from "../Button";
+import Watch from "./Watch";
+import style from "./Stopwatch.module.scss";
 
-export function Stopwatch() {
+export default function Stopwatch() {
   return (
-    <>
-      <div>
-        <p>Choose a card and start the stopwatch</p>
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Choose a card and start the stopwatch</p>
+      <div className={style.relogioWrapper}>
+        <Watch />
       </div>
+
       <div>
         <Button>Begin</Button>
       </div>
-    </>
+    </div>
   );
 }
